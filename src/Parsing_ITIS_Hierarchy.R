e0137dbbb5b4d7ceb2d46c798254f5c79f720bf6
HeartlandNetwork <gareth_rowell@nps.gov>
2023-03-09 08:51:46 -0600

    parsing includes inner joins

--- src/Parsing_ITIS_Hierarchy.R
+++ src/Parsing_ITIS_Hierarchy.R
@@ -46,15 +46,127 @@ my_columns
 longnames
 
 # convert all character to char to numeric
-my_numbers <- my_columns %>% mutate_if(where(is.character), as.double)
+my_numbers <- my_columns|> mutate_if(where(is.character), as.double)
 
 my_columns
 
 my_numbers
+ 
+
+# Joins on tsn
+
+longnames |> 
+  count(tsn) |> 
+  filter(n > 1)
+
+########## completename from col1
+
+my_numbers_col1 <- rename(my_numbers, tsn = col1)
+
+my_numbers_col1 
+
+my_join_col1 = my_numbers_col1 |> 
+  inner_join(longnames, join_by(tsn))
+
+my_join_col1
+
+############## more completenames
+
+my_numbers_col2 <- rename(my_numbers, tsn = col2)
+
+my_numbers_col2 
+
+my_join_col2 = my_numbers_col2 |> 
+  inner_join(longnames, join_by(tsn))
+
+my_join_col2
+
+############## more completenames
+
+my_numbers_col3 <- rename(my_numbers, tsn = col3)
+
+my_numbers_col3 
+
+my_join_col3 = my_numbers_col3 |> 
+  inner_join(longnames, join_by(tsn))
+
+my_join_col3
+
+############## more completenames
+
+my_numbers_col4 <- rename(my_numbers, tsn = col4)
+
+my_numbers_col4 
+
+my_join_col4 = my_numbers_col4 |> 
+  inner_join(longnames, join_by(tsn))
+
+my_join_col4
+
+############## more completenames
+
+my_numbers_col5 <- rename(my_numbers, tsn = col5)
+
+my_numbers_col5 
+
+my_join_col5 = my_numbers_col5 |> 
+  inner_join(longnames, join_by(tsn))
+
+my_join_col5
 
 
-# Then set PK in longnames to be tsn
+############## more completenames
 
-# Then process each column as an FK
-# to get the value for complete name from longnames
+my_numbers_col6 <- rename(my_numbers, tsn = col6)
+
+my_numbers_col6 
+
+my_join_col6 = my_numbers_col6 |> 
+  inner_join(longnames, join_by(tsn))
+
+my_join_col6
+
+
+############## more completenames
+
+my_numbers_col7 <- rename(my_numbers, tsn = col7)
+
+my_numbers_col7 
+
+my_join_col7 = my_numbers_col7 |> 
+  inner_join(longnames, join_by(tsn))
+
+my_join_col7
+
+############## more completenames
+
+my_numbers_col8 <- rename(my_numbers, tsn = col8)
+
+my_numbers_col8 
+
+my_join_col8 = my_numbers_col8 |> 
+  inner_join(longnames, join_by(tsn))
+
+my_join_col8
+
+
+############## more completenames
+
+my_numbers_col9 <- rename(my_numbers, tsn = col9)
+
+my_numbers_col9 
+
+my_join_col9 = my_numbers_col9 |> 
+  inner_join(longnames, join_by(tsn))
+
+my_join_col9
+
+
+
+
+
+
+
+
+
 
