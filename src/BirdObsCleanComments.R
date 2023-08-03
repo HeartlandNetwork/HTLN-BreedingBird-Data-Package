2c1ba02a725c2a1f0397c1ba54b1a8ceb6e3c54c
HeartlandNetwork <gareth_rowell@nps.gov>
2023-08-03 15:24:11 -0500

    cleaning comments

--- src/BirdObsCleanComments.R
+++ src/BirdObsCleanComments.R
@@ -1,18 +1,17 @@
 
 library(tidyverse)
-# library(data.table)
 
 birdobs <- read_csv("src/BirdObservationsThru2022_3.csv")
 
 glimpse(birdobs)
 
 
-
-# just searching for stuff that starts with "W"
+# look at unique comments
 
 birdobs |>  
   select(Comments) |> 
-  filter(str_detect(Comments, "^W"))
+  distinct(Comments) |>
+  print(n = 909)
 
 
 # try search for a special character "@"
@@ -21,8 +20,73 @@ birdobs |>
   select(Comments) |> 
   filter(str_detect(Comments, "@"))
 
-# try search for some patterns
+# replace @ with ' at '
+birdobs <- birdobs |>  
+  mutate(
+    Comments = str_replace_all(Comments, "@", "at"))
+
+# check for @ after mutate
 
 birdobs |>  
   select(Comments) |> 
-  filter(str_detect(Comments, "^a"))
+  filter(str_detect(Comments, "heard at creek crossing"))
+
+# pass Comments2 to Comments1 in birdobs
+
+# look for other special chars
+
+birdobs |>  
+  select(Comments) |> 
+  distinct(Comments) |>
+  print(n = 909)
+
+# try search for a special character "~"
+
+birdobs |>  
+  select(Comments) |> 
+  filter(str_detect(Comments, "~"))
+
+# replace ~  with 'ca.'
+birdobs <- birdobs |>  
+  mutate(
+    Comments = str_replace_all(Comments, "~", "ca."))
+
+# check for tilde after mutuate
+
+birdobs |>  
+  select(Comments) |> 
+  filter(str_detect(Comments, "100 meters"))
+
+# try search for a special character "&"
+
+birdobs |>  
+  select(Comments) |> 
+  filter(str_detect(Comments, "&"))
+
+# replace &  with 'and'
+birdobs <- birdobs |>  
+  mutate(
+    Comments = str_replace_all(Comments, "&", "and"))
+
+# check for & after mutuate
+
+birdobs |>  
+  select(Comments) |> 
+  filter(str_detect(Comments, "corrals"))
+
+
+
+
+# look for other special chars
+
+birdobs |>  
+  select(Comments) |> 
+  distinct(Comments) |>
+  print(n = 909)
+
+
+write_csv(birdobs, "src/BirdObservationsThru2022_3b.csv")
+
+
+
+  
