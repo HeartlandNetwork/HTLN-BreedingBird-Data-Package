b72b495dba61485c876799b4defd47018c8a3b7e
HeartlandNetwork <gareth_rowell@nps.gov>
2023-08-10 16:01:04 -0500

    make_eml errors

--- EML/NPS-EML-Creation-Workflow-HTLNBirds.R
+++ EML/NPS-EML-Creation-Workflow-HTLNBirds.R
@@ -44,7 +44,7 @@
 
 library(NPSdataverse)
 library(tidyverse)
-library(EML)
+
 
 ############################################
 # OBJECT ASSIGNMENT
@@ -317,6 +317,8 @@ enddate <- ymd("2022-06-15")
 # at the end of the process to get feedback on items that might be missing or need 
 # attention. Fix these issues and then re-run the `make_eml()` function.
 
+# library(EML)
+
 working_folder <- setwd("C:/users/growell/HTLN-BreedingBird-Data-Package/EML")
 
 
@@ -332,7 +334,7 @@ my_metadata <- make_eml(path = working_folder,
                return.obj = TRUE, 
                write.file = FALSE)
 
-EML::write_eml(my_metadata, “my_metadata.xml”
+EML::write_eml(my_metadata, “my_metadata.xml”)
 
 ## Check for EML validity 
 #This is a good point to pause and test whether your EML is valid. 
